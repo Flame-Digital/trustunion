63324f182f56535a7282b41e2f8c7ca3557a133c
edgiek-altrp <edgiek@academ-x.ru>
2022-05-20 18:56:05 +0300

    Push new_sc

--- new_tokenSwap.sol
+++ new_tokenSwap.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.13;
 
 import "./TRUSTT.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
