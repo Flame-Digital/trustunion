f6b7ece63717efda4db5510e89902b2fa7a2f881
edgiek-altrp <edgiek@academ-x.ru>
2022-07-12 23:13:28 +0300

    Add change owner for router

--- router.sol
+++ router.sol
@@ -1,6 +1,80 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.14;
 
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
 
 interface IERC20 {
     function transfer(address to, uint256 value) external returns (bool);
@@ -8,20 +82,16 @@ interface IERC20 {
     function approve(address spender, uint256 amount) external returns (bool);
 }
 
-contract router {
+contract router is Ownable{
 
     function routTokens(IERC20 token, address[] memory recipients, uint256[] memory values, uint amount) external {
-
-        require(address(token) != address(0), "Token address is null address!");
-
-        require(recipients.length == values.length, "recipients.length != values.length!");
 
         uint sum = 0;
 
         for(uint a = 0; a < values.length; a++)
           sum = sum + values[a];
 
-        require(sum == amount, "Values != amount!");
+        require(sum == amount, "Values != amount");
 
         token.transferFrom(msg.sender, address(this), amount);
 
